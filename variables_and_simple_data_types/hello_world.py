4eacc5cd8f8985b19b324f4068c2c89e46e09949
Enki Layman <layman212@gmail.com>
2024-06-21 03:37:12 -0400

    exercise 2.1

---
+++ variables_and_simple_data_types/hello_world.py
@@ -0,0 +1,20 @@
+message = "Hello world!"
+print(message)
+
+message = "Hello Python Crash Course"
+print(message) 
+
+# To change the filepath in the terminal type cd filepath
+# To see what folders/files exist within the current filepath type dir
+# To execute a file in the terminal type python filename
+
+# When an error occurs a traceback is output to the terminal.
+# The first part tells you which file the error occured, as well as what line of the file. 
+# The interpreter then gives a more specific hint toward what caused the error
+# Lastly the error type is given, such as syntax error, name error, etc.
+
+# A name error ususally means a value was not given to the variable, or we spelled the variable name wrong.
+
+# Variables should not be looked at as storage for values, but as labels/names for that value, so that we have a way to 
+# reference it. 
+
