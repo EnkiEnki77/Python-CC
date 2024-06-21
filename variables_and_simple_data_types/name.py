51a3bde2c9fb2c06200765b22682ed2c9f184235
Enki Layman <layman212@gmail.com>
2024-06-21 04:05:15 -0400

    name.py

---
+++ variables_and_simple_data_types/name.py
@@ -0,0 +1,19 @@
+name = "ada lovelace"
+print(name.title())
+name = "ADA LOVELACE"
+print(name.title())
+
+
+# The title string method converts a string to title case. Where the first letter of each word is uppcase, the rest are 
+# lower. Its useful for when you want to format user input consistently the same way. So that input is case insensitive.
+
+# A method is an action python can permorm on a piece of data. A method is a function.
+# The dot tells the method to act on a certain variable. 
+
+# Some other case methods are: 
+print(name.upper())
+print(name.lower())
+
+# The lower method is especially useful when storing data. You often dont want to trust a users casing on input. So you
+# store it as lower case, and then use the appropriate case method when the time comes to display the data.
+
