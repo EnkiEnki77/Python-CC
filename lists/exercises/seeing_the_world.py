ac1b1a8fd1528913e01ddc639236a27dd1297ae7
Enki Layman <layman212@gmail.com>
2024-06-22 02:01:49 -0400

    exercise 3.8

---
+++ lists/exercises/seeing_the_world.py
@@ -0,0 +1,15 @@
+places_to_see = ["Sweden", "Japan", "Germany", "Thailand", "Australia"]
+
+print(places_to_see)
+print(sorted(places_to_see))
+print(places_to_see)
+print(sorted(places_to_see, reverse=True))
+print(places_to_see)
+places_to_see.reverse()
+print(places_to_see)
+places_to_see.reverse()
+print(places_to_see)
+places_to_see.sort()
+print(places_to_see)
+places_to_see.sort(reverse=True)
+print(places_to_see)
