9d3a3b2bc67420338b26ac67ed9ac0612a09ec54
Enki Layman <layman212@gmail.com>
2024-06-22 04:20:21 -0400

    if statements

---
+++ if_statements/banned_users.py
@@ -0,0 +1,12 @@
+users = ["Enki", "Bat", "Devin", "Hari"]
+
+# To check if an item exists in a list use the in keyword. it returns a boolean value
+print('Enki' in users)
+
+# To check if an item is not use not in
+print('Dad' not in users)
+
+# Boolean values are an important way to track the state or particular conditions important to your program. such as
+# whether a game is active or a user can edit something.
+
+
