ba6ac76f4568b308e5a415480641818795572aa2
Enki Layman <layman212@gmail.com>
2024-06-22 02:04:57 -0400

    seeing the world

--- lists/exercises/seeing_the_world.py
+++ lists/exercises/seeing_the_world.py
@@ -13,3 +13,7 @@ places_to_see.sort()
 print(places_to_see)
 places_to_see.sort(reverse=True)
 print(places_to_see)
+
+# If you want to access the last item in a list always use -1 for the index.
+
+places_to_see[-1]
