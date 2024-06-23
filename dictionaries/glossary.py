f2b9c74528a661845a19c47408e87fd959581438
Enki Layman <layman212@gmail.com>
2024-06-23 00:33:40 -0400

    exercise 6.3

---
+++ dictionaries/glossary.py
@@ -0,0 +1,10 @@
+glossary = {
+    'slice': 'a copy of all of or part of a list',
+    'variable': 'label that data is assigned to so that it can be referenced in memory',
+    'list': 'collection of data that is ordered and accessible by index starting from 0',
+    'boolean': 'a value of either true or false, used in conditional statements',
+    'f-string': 'format string, a string that allows you to embed variables/expressions'
+}
+
+for word, meaning in glossary.items():
+    print(f'{word}: {meaning}\n')
