8e109121c885b21f75aaaf50dd6a32da475535bf
Enki Layman <layman212@gmail.com>
2024-06-24 04:27:20 -0400

    lottery analysis program

--- classes/lottery.py
+++ classes/lottery.py
@@ -1,12 +1,35 @@
-from random import randint
+from random import randint, choice
 
-lottery = [1, 3, 42, 12, 51, 63, 24, 23, 64, 41, 'a', 'e', 'd', 'y', 'j']
+import string
 
-winning_lot = ''
-
-for num in range(1, 5):
-    # print(randint(0, len(lottery) - 1))
+def generate_possible_chars():
+    """Generates the list of possible characters that will be picked from for the winning lot"""
+    # Get a string of all the lowercase letters in the alphabet
+    alphabet = string.ascii_uppercase
+    # Generate a list of the nums from 1 - 100
+    possible_chars = list(range(1, 101))
+    # Append the letters of the alphabet to the list of numbers
+    for letter in alphabet:
+        possible_chars.append(letter)
     
-    winning_lot += f"{str(lottery[randint(0, len(lottery) - 1)]).title()}, "
+    return possible_chars
 
-print(f"Anyone who gets a lottery ticket with these numbers/letters wins!: {winning_lot[:-2]}")
+def create_lot():
+    """Function that generates a 6-digit winning lot of numbers/letters"""
+
+    # Generates list of possible chars for winning lot
+    possible_chars = generate_possible_chars()
+
+    # String that will be returned as the winning lot
+    lot = ''
+    # Randomly picks the six characters for the winning lot from the possible characters.
+    for char in range(1, 3):
+        lot += f"{str(choice(possible_chars))}, "
+
+    print(lot[:-2])
+    # Formats the winning lot string correctly and then returns it
+    return lot[:-2]
+
+# create_lot()
+        
+
---
+++ classes/lottery_analysis.py
@@ -0,0 +1,33 @@
+from lottery import create_lot
+
+import string
+
+
+def lottery_difficulty_analysis():
+    """Analyzes how many tickets it takes to win the lottery"""
+
+    # Generates the winning lottery chars
+    winning_lot = create_lot()
+    # Declares the chars needed to win the lottery 
+    print(f"The numbers you need on your ticket to win are: {winning_lot}")
+
+    # Keeps track of how many tickets it took to win
+    tickets_to_win = 0
+
+    # The current lottery ticket being compared to the winning numbers
+    current_ticket = None
+    # Generates lottery tickets until one matches the winning numbers
+    while current_ticket != winning_lot:
+        current_ticket = create_lot()
+        tickets_to_win +=1
+        print(f"The winning lot was {winning_lot}, your ticket was {current_ticket}. {tickets_to_win} tickets have been used")
+
+        if tickets_to_win == 100_000:
+            print(f"100,000 tickets have been bought, and still no winner!")
+            return
+
+    # returns number of tickets it took to win
+    print(f"\nIt took {tickets_to_win} tickets to win the lottery.")
+
+
+lottery_difficulty_analysis()
